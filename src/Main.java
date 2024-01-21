09e1a4d69b91867d56df5dbf5044600b2907b317
lvarezhkal <kkakxaa@gmail.com>
2024-01-21 23:10:30 +0300

    Objects and classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("Книга 1");
+        Author king = new Author("Стивен", "Кинг");
+        Book theShining = new Book("Сияние", 1977);
+        System.out.println(theShining.getNames() + " " + king.getName() + " " + king.getLastName() + " " + theShining.getYear());
+        System.out.println("Книга 2");
+        Author austen = new Author("Джейн", "Остин");
+        Book persuasion = new Book("Доводы рассудка", 1817);
+        System.out.println(persuasion.getNames() + " " + austen.getName() + " " + austen.getLastName() + " " + persuasion.getYear());
+        persuasion.setYear(1820);
+        System.out.println(persuasion.getNames() + " " + austen.getName() + " " + austen.getLastName() + " " + persuasion.getYear());
     }
 }
